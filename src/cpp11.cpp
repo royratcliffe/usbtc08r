c5cc36c007c1e5a889725e0847d6669a38a67f5a
Roy Ratcliffe <roy@ratcliffe.me>
2022-09-12 21:07:21 +0100

    docs: roxygen

--- src/cpp11.cpp
+++ src/cpp11.cpp
@@ -6,6 +6,13 @@
 #include <R_ext/Visibility.h>
 
 // code.cpp
+std::string error_(int16_t error);
+extern "C" SEXP _usbtc08r_error_(SEXP error) {
+  BEGIN_CPP11
+    return cpp11::as_sexp(error_(cpp11::as_cpp<cpp11::decay_t<int16_t>>(error)));
+  END_CPP11
+}
+// code.cpp
 int16_t open_();
 extern "C" SEXP _usbtc08r_open_() {
   BEGIN_CPP11
@@ -27,10 +34,10 @@ extern "C" SEXP _usbtc08r_open_progress_() {
   END_CPP11
 }
 // code.cpp
-int16_t set_channel_(int16_t handle, int16_t channel, int8_t tc_type);
+int16_t set_channel_(int16_t handle, int16_t channel, char tc_type);
 extern "C" SEXP _usbtc08r_set_channel_(SEXP handle, SEXP channel, SEXP tc_type) {
   BEGIN_CPP11
-    return cpp11::as_sexp(set_channel_(cpp11::as_cpp<cpp11::decay_t<int16_t>>(handle), cpp11::as_cpp<cpp11::decay_t<int16_t>>(channel), cpp11::as_cpp<cpp11::decay_t<int8_t>>(tc_type)));
+    return cpp11::as_sexp(set_channel_(cpp11::as_cpp<cpp11::decay_t<int16_t>>(handle), cpp11::as_cpp<cpp11::decay_t<int16_t>>(channel), cpp11::as_cpp<cpp11::decay_t<char>>(tc_type)));
   END_CPP11
 }
 // code.cpp
@@ -48,24 +55,24 @@ extern "C" SEXP _usbtc08r_stop_(SEXP handle) {
   END_CPP11
 }
 // code.cpp
-cpp11::data_frame get_temp_(int16_t handle, int32_t length, int16_t channel, int16_t units, int16_t fill);
+cpp11::data_frame get_temp_(int16_t handle, int32_t length, int16_t channel, std::string units, bool fill);
 extern "C" SEXP _usbtc08r_get_temp_(SEXP handle, SEXP length, SEXP channel, SEXP units, SEXP fill) {
   BEGIN_CPP11
-    return cpp11::as_sexp(get_temp_(cpp11::as_cpp<cpp11::decay_t<int16_t>>(handle), cpp11::as_cpp<cpp11::decay_t<int32_t>>(length), cpp11::as_cpp<cpp11::decay_t<int16_t>>(channel), cpp11::as_cpp<cpp11::decay_t<int16_t>>(units), cpp11::as_cpp<cpp11::decay_t<int16_t>>(fill)));
+    return cpp11::as_sexp(get_temp_(cpp11::as_cpp<cpp11::decay_t<int16_t>>(handle), cpp11::as_cpp<cpp11::decay_t<int32_t>>(length), cpp11::as_cpp<cpp11::decay_t<int16_t>>(channel), cpp11::as_cpp<cpp11::decay_t<std::string>>(units), cpp11::as_cpp<cpp11::decay_t<bool>>(fill)));
   END_CPP11
 }
 // code.cpp
-cpp11::data_frame get_temp_deskew_(int16_t handle, int32_t length, int16_t channel, int16_t units, int16_t fill);
+cpp11::data_frame get_temp_deskew_(int16_t handle, int32_t length, int16_t channel, std::string units, bool fill);
 extern "C" SEXP _usbtc08r_get_temp_deskew_(SEXP handle, SEXP length, SEXP channel, SEXP units, SEXP fill) {
   BEGIN_CPP11
-    return cpp11::as_sexp(get_temp_deskew_(cpp11::as_cpp<cpp11::decay_t<int16_t>>(handle), cpp11::as_cpp<cpp11::decay_t<int32_t>>(length), cpp11::as_cpp<cpp11::decay_t<int16_t>>(channel), cpp11::as_cpp<cpp11::decay_t<int16_t>>(units), cpp11::as_cpp<cpp11::decay_t<int16_t>>(fill)));
+    return cpp11::as_sexp(get_temp_deskew_(cpp11::as_cpp<cpp11::decay_t<int16_t>>(handle), cpp11::as_cpp<cpp11::decay_t<int32_t>>(length), cpp11::as_cpp<cpp11::decay_t<int16_t>>(channel), cpp11::as_cpp<cpp11::decay_t<std::string>>(units), cpp11::as_cpp<cpp11::decay_t<bool>>(fill)));
   END_CPP11
 }
 // code.cpp
-cpp11::logicals get_single_(int16_t handle, int16_t units);
+cpp11::logicals get_single_(int16_t handle, std::string units);
 extern "C" SEXP _usbtc08r_get_single_(SEXP handle, SEXP units) {
   BEGIN_CPP11
-    return cpp11::as_sexp(get_single_(cpp11::as_cpp<cpp11::decay_t<int16_t>>(handle), cpp11::as_cpp<cpp11::decay_t<int16_t>>(units)));
+    return cpp11::as_sexp(get_single_(cpp11::as_cpp<cpp11::decay_t<int16_t>>(handle), cpp11::as_cpp<cpp11::decay_t<std::string>>(units)));
   END_CPP11
 }
 // code.cpp
@@ -80,13 +87,6 @@ int16_t get_last_error_(int16_t handle);
 extern "C" SEXP _usbtc08r_get_last_error_(SEXP handle) {
   BEGIN_CPP11
     return cpp11::as_sexp(get_last_error_(cpp11::as_cpp<cpp11::decay_t<int16_t>>(handle)));
-  END_CPP11
-}
-// code.cpp
-std::string error_(int16_t error);
-extern "C" SEXP _usbtc08r_error_(SEXP error) {
-  BEGIN_CPP11
-    return cpp11::as_sexp(error_(cpp11::as_cpp<cpp11::decay_t<int16_t>>(error)));
   END_CPP11
 }
 
