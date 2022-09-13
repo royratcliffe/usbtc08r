98c12d610b51ef9a83f59504b02d4e689a72e33e
Roy Ratcliffe <roy@ratcliffe.me>
2022-09-13 09:22:05 +0100

    feat: usb_tc08_open_async_complete

--- R/code.R
+++ R/code.R
@@ -1,4 +1,4 @@
-#' Opens TC-08 unit for synchronous-mode operations
+#' Opens TC-08 unit for synchronous-mode operations.
 #' @description Opens the next available closed TC-08 unit. Opens synchronously
 #'   and therefore blocks the caller until a USB-connected TC-08 has been found
 #'   and opened; a process that might take a second or two.
@@ -17,7 +17,7 @@ usb_tc08_open <- function() {
   if (handle > 0L) structure(handle, class = c("sync_tc08", "tc08"))
 }
 
-#' Starts opening TC-08 units asynchronously
+#' Starts opening TC-08 units asynchronously.
 #' @description Asynchronously opens all as-yet-unopened TC-08 units.
 #'   Automatically asserts success by stopping on error.
 #' @details The underlying driver must return 1 as success. All other return
@@ -26,25 +26,44 @@ usb_tc08_open <- function() {
 #'   Open only once.
 #' @export
 #' @examples
-#' \dontrun{
+#' \donttest{
 #' library(usbtc08r)
 #' usb_tc08_open_async()
 #' }
 usb_tc08_open_async <- function()
   if (!open_async_()) stop(error_(get_last_error_(0L)))
 
-#' Progressively opens TC-08 units for asynchronous-mode operations
+#' Progressively opens TC-08 units for asynchronous-mode operations.
+#' @return Progress string, one of: pending or complete. Does \emph{not} return
+#'   \code{"fail"} but raises an exception on failure by asking for the last
+#'   error and decoding it for the exception message.
 #' @export
 usb_tc08_open_progress <- function() {
   progress <- open_progress_()
   if (progress == "fail") stop(error_(get_last_error_(0L)))
   handle <- attr(progress, "handle")
+  attr(progress, "handle") <- NULL
   if (handle > 0L)
     attr(progress, "tc08") <- structure(handle, class = c("async_tc08", "tc08"))
   progress
 }
 
-#' Sets a channel thermocouple type
+#' Opens all TC-08 units asynchronously to completion.
+#' @details Runs aggressively with \emph{no} delay between pending progress
+#'   results.
+#' @return List of TC-08 units opened and ready for set up and running.
+usb_tc08_open_async_complete <- function() {
+  tc08 <- list()
+  repeat {
+    progress <- usb_tc08_open_progress()
+    tc08[[length(tc08) + 1L]] <- attr(progress, "tc08")
+    if (progress != "pending") break
+  }
+  stopifnot(progress == "complete")
+  tc08
+}
+
+#' Sets a channel thermocouple type.
 #' @details Set up the channels \emph{before} running in asynchronous mode.
 #' @param x TC-08 unit, synchronous or asynchronous mode.
 #' @param channel Channel to set.
@@ -58,7 +77,7 @@ usb_tc08_set_channel <- function(x, channel, tc_type) UseMethod("usb_tc08_set_ch
 #' @export
 usb_tc08_set_channel.tc08 <- function(x, ...) set_channel_(x, ...)
 
-#' Runs a TC-08 asynchronously
+#' Runs a TC-08 asynchronously.
 #' @details Only succeeds when in asynchronous mode. Fails when synchronous. Use
 #'   \code{\link[usbtc08r]{usb_tc08_stop}} to stop the run, before closing.
 #' @param x TC-08 opened for asynchronous-mode operations.
@@ -70,7 +89,7 @@ usb_tc08_run <- function(x, interval) UseMethod("usb_tc08_run", x)
 #' @export
 usb_tc08_run.async_tc08 <- function(x, ...) run_(x, ...)
 
-#' Stops an asynchronous TC-08
+#' Stops an asynchronous TC-08.
 #' @param x Opened and running TC-08 in asynchronous mode.
 #' @export
 usb_tc08_stop <- function(x) UseMethod("usb_tc08_stop", x)
@@ -78,7 +97,7 @@ usb_tc08_stop <- function(x) UseMethod("usb_tc08_stop", x)
 #' @export
 usb_tc08_stop.async_tc08 <- function(x, ...) stop_(x, ...)
 
-#' Get temperatures asynchronously
+#' Get temperatures asynchronously.
 #' @param x Opened and running TC-08 in asynchronous mode.
 #' @export
 usb_tc08_get_temp <- function(x) UseMethod("usb_tc08_get_temp", x)
@@ -86,7 +105,7 @@ usb_tc08_get_temp <- function(x) UseMethod("usb_tc08_get_temp", x)
 #' @export
 usb_tc08_get_temp.async_tc08 <- function(x, ...) get_temp_(x, ...)
 
-#' Get asynchronously-captured de-skewed temperatures
+#' Get asynchronously-captured de-skewed temperatures.
 #' @param x Opened and running TC-08 in asynchronous mode.
 #' @export
 usb_tc08_get_temp_deskew <- function(x) UseMethod("usb_tc08_get_temp_deskew", x)
@@ -94,7 +113,7 @@ usb_tc08_get_temp_deskew <- function(x) UseMethod("usb_tc08_get_temp_deskew", x)
 #' @export
 usb_tc08_get_temp_deskew.async_tc08 <- function(x, ...) get_temp_deskew_(x, ...)
 
-#' Synchronously samples all enabled channels
+#' Synchronously samples all enabled channels.
 #' @param x Opened synchronous-mode TC-08 unit.
 #' @param units Units for resulting temperatures.
 #' @return Logically \code{TRUE} on success. The logical vector has two
@@ -111,7 +130,7 @@ usb_tc08_get_single <- function(x, units) UseMethod("usb_tc08_get_single", x)
 #' @export
 usb_tc08_get_single.sync_tc08 <- function(x, ...) get_single_(x, ...)
 
-#' Closes TC-08 unit
+#' Closes TC-08 unit.
 #' @param x Previously opened TC-08 unit.
 #' @details Once closed externally, the handle becomes invalid.
 #' @export
@@ -120,7 +139,7 @@ usb_tc08_close <- function(x) UseMethod("usb_tc08_close", x)
 #' @export
 usb_tc08_close.tc08 <- function(x) if (close_(x) < 1L) stop(error_(get_last_error_(x)))
 
-#' Gets last error of TC-08 unit
+#' Gets last error of TC-08 unit.
 #' @param x Open TC-08 unit.
 #' @export
 usb_tc08_get_last_error <- function(x) UseMethod("usb_tc08_get_last_error", x)
